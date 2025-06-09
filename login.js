a1b4e6f17f4187da7d918673b613fe8a99ec06c0
Panomwat Sangsuwan <pnmjj_@hotmail.com>
2025-06-09 16:27:26 +0700

    fix

--- login.js
+++ login.js
@@ -1,15 +1,20 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
   event.preventDefault(); 
-  
-  const email = document.getElementById('email').value.trim();
-  const password = document.getElementById('password').value.trim();
-  const message = document.getElementById('message');
 
-  if (email === '' || password === '') {
-    message.style.color = 'red';
-    message.textContent = 'กรุณากรอกข้อมูลให้ครบทุกช่อง';
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const message = document.getElementById("message");
+
+
+  if (email === "test@example.com" && password === "123456") {
+    message.style.color = "green";
+    message.innerText = "Login สำเร็จ!";
   } else {
-    message.style.color = 'green';
-    message.textContent = 'เข้าสู่ระบบสำเร็จ!';
+    message.style.color = "red";
+    message.innerText = "Email หรือ Password ไม่ถูกต้อง";
   }
 });
+
+setTimeout(() => {
+      window.location.href = "index.html";
+    }, 1000);
